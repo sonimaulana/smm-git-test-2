b0ba2db465276a423b4123d34171649015af8913
sonimaulana <sonimaulanam11@gmail.com>
2023-09-16 09:07:42 +0700

    Menambahkan Ul li

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
     <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi quis, nihil sapiente magni quam nesciunt quas quidem et quo vel, consectetur dolorum veritatis vero ullam totam possimus qui repellat quos.
     </p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
